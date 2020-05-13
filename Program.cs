055f93295936872e748b0da76adee1ee51eaf985
LipeClash <felipeclaudino2003@hotmail.com>
2020-05-12 23:54:33 -0300

    feito

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace auladepc1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite seu primeiro nome");
+            String Nome = Console.ReadLine();
+
+            Console.WriteLine("Digite seu sobrenome");
+            String Sobrenome = Console.ReadLine();
+        
+            Console.WriteLine(Nome + " " +  Sobrenome);
+
+             Console.WriteLine("Digite seu sobrenome");
+             String CatálagoSobrenome = Console.ReadLine();
+
+             Console.WriteLine("Digite seu primeiro nome");
+             String CatálagoNome = Console.ReadLine();
+
+             Console.WriteLine(CatálagoSobrenome + " " + CatálagoNome);
+
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # NomeCompleto
 15/05/2020
+
+feito
